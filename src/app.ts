5c54866a3cdfc3e4b1f290364d1c4ed9777ac4cf
Emeric FOKOUONG TOUOLAC <bestvemeric@gmail.com>
2022-06-21 13:32:51 -0500

    third_parties done

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-console.log("THis is a typescript file, (AN unpdated message)");
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+import { Product } from "./product.models";
+import { validate } from "class-validator";
+
+const products = [
+  { title: "a carpet", price: 29.99 },
+  { title: "a cart", price: 19.99 },
+];
+
+const p1 = new Product("A BOOK", 13.59);
+const newProd = new Product("", -4.56);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("validation errors!");
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
+
+console.log(p1.getInformation());
+const loadedProducts = plainToClass(Product, products);
+
+loadedProducts.forEach((prod) => {
+  console.log(prod.getInformation());
+});
